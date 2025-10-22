eed4b8eb23e0e9463eb41a545053eee8479f7b44
usiel667 <usiel667@users.noreply.github.com>
2025-10-21 23:08:33 -0600

    added lib queries, customer form page

---
+++ app/(rs)/customers/form/page.tsx
@@ -0,0 +1,33 @@
+import { getCustomer } from "@/lib/queries/getCustomer";
+
+export default async function CustomerFormPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  try {
+    const { customerId } = await searchParams;
+
+    // Edit customer form
+    if (customerId) {
+      const customer = await getCustomer(parseInt(customerId));
+
+      if (!customer) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer ID #{customerId} not found
+            </h2>
+          </>
+        );
+      }
+      // put customer form component here
+    } else {
+      // put new customer form component here
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      throw e;
+    }
+  }
+}
